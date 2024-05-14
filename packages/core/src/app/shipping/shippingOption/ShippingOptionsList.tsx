bfbf2895064f1bf32939f96d4c171c3656be0fa4
orlando-space <orlando@space.bar>
2024-05-14 17:12:45 -0500

    Show "nosotros mismos" carrier

--- packages/core/src/app/shipping/shippingOption/ShippingOptionsList.tsx
+++ packages/core/src/app/shipping/shippingOption/ShippingOptionsList.tsx
@@ -99,8 +99,6 @@ const ShippingOptionsList: FunctionComponent<ShippingOptionListProps> = ({
 
             if(!filteredShipping) continue;
 
-            if(filteredShipping.description === "NOSOTROS MISMOS") continue;
-
             const isTheStateAndSelanusasGroup = customerGroupId===570 && stateOrProvince === 'Ciudad de México'
 
             // has to be one of the selected pickup in store, has the correcto postalCode and be in the state México
